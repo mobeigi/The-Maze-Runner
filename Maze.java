97917c5a77f91ebf341514fa24cc625d26ce29c2
Gavin Tam <gavintam168@gmail.com>
2014-05-04 11:39:24 +1000

    Included Maze.java

---
+++ Maze.java
@@ -0,0 +1,49 @@
+/**
+ * Contains the board of the maze.
+ * @author Gavin Tam
+ * @see Tile
+ * @see Player
+ */
+public class Maze {
+	private Tile[][] grid;
+	private int width;
+	private int height;
+			
+	public Maze (int width, int height) {
+		grid = new Tile[width][height];
+		this.width = width;
+		this.height = height;
+	}
+	
+	public void createMaze() {
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				Tile t; 
+				//if border tile
+				if (i == 0 || i == width-1 ||
+					j == 0 || j == height-1) {
+					t = new Tile(0); // 0 denotes walkable tile
+				} else {
+					t = new Tile(1); // 1 denotes wall/non-walkable tile
+				}
+				grid[i][j] = t;
+			}
+		}
+	}
+	
+	/**
+	 * Finds the tile on the maze that the player is situated in
+	 * @param p the player
+	 * @return the tile in which the player is on
+	 */
+	public Tile findPlayer (Player p) {
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				if (grid[i][j].containsPlayer(p)) {
+					return grid[i][j];
+				}
+			}
+		}
+		return null;
+	}
+}
