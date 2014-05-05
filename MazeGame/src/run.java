fff3edc65395ac50c8fe56da6eab903ba6ce299a
mohammadg <webmaster@mohammadg.com>
2014-05-05 20:53:08 +1000

    Empty Classes discussed for Design

---
+++ MazeGame/src/run.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class run 
+{
+	public static void main (String args[])
+	{
+		Game g = new Game();
+		
+		while (//g.playAgain)
+		{
+			//g.createGame();
+			
+			while (//!g.isGameOver())
+			{
+				//g.updateGame();
+				//g.drawGame();
+			}
+			
+			Scanner s = new Scanner(System.in);
+			
+			if (s.next().equalsIgnoreCase("Y") || s.next().equalsIgnoreCase("YES"))
+				//g.setPlayAgain(s.next());
+		}
+	}
+
+}
