0a63e5348896a7d70708a626cfec4a0545027b61
ckonethingy <calvinflinders3@hotmail.com>
2014-05-08 23:55:24 +1000

    Updated Tile.java

--- MazeGame/src/Tile.java
+++ MazeGame/src/Tile.java
@@ -1,4 +1,24 @@
 
 public class Tile {
+	private int x;
+	private int y;
+	private boolean isWalkable;
+	
+	public Tile (boolean isWalkable, int x, int y){
+		this.x = x;
+		this.y = y;
+		this.isWalkable = isWalkable;
+	}
 
+	public void setWalkable (boolean isWalkable){
+		this.isWalkable = isWalkable;
+	}
+	
+	public int getX(){
+		return this.x;
+	}
+
+	public int getY(){
+		return this.y;
+	}
 }
