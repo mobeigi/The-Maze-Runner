d92ea9080aab66d489a617f2f06293950f8efea5
ckonethingy <calvinflinders3@hotmail.com>
2014-05-08 23:58:43 +1000

    Moved Gavin's Maze and TileEdge to src file

--- MazeGame/src/Maze.java
+++ MazeGame/src/Maze.java
@@ -1,4 +1,66 @@
+/**
+ * Contains the board of the maze.
+ * It contains the maze-generating algorithm.
+ * @author Gavin Tam
+ * @see Tile
+ * @see Player
+ */
 
-public interface Maze {
-
+public class Maze {
+	private Tile[][] grid;
+	private int width;
+	private int height;
+	private Tile playerLoc;	//location of the current player
+			
+	public Maze (int width, int height) {
+		grid = new Tile[width][height];
+		this.width = width;
+		this.height = height;
+		createMaze();	//initialise all tiles
+		playerLoc = grid[0][1];	//origin at (0,1), 
+								//width and height should be big enough to allow this to be valid
+	}
+	
+	public void createMaze() {
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				Tile t; 
+				//if border tile
+				//not origin which is (0,1)
+				//not destination which is (width-1, height)
+				if ((i == 0 && j != 1) || (i == width-1 && j != height) ||
+					j == 0 || j == height-1) {
+					t = new Tile(false,i,j); // false denotes walkable tile
+				} else {
+					t = new Tile(true,i,j); // true denotes wall/non-walkable tile
+				}
+				grid[i][j] = t;
+			}
+		}
+	}
+	
+	/**
+	 * Finds the tile on the maze that the player is situated in
+	 * @param p the player
+	 * @return the tile in which the player is on
+	 */
+	public Tile findPlayer (Player p) {
+		return playerLoc;
+	}
+	
+	public void updatePlayerLoc (int x, int y) {
+		//only update player location if the move is valid
+		//disallow teleportation? (check here or elsewhere?) 
+		//e.g. limit x to 0,1,-1 and y to 0,1,-1 and one of x,y must be 0
+		//as we can only move in one direction at a time
+		if (playerLoc.getX()+x < width && playerLoc.getY()+y < height
+			&& grid[playerLoc.getX()+x][playerLoc.getY()+y].isWalkable()) {
+			playerLoc = grid[playerLoc.getX()+x][playerLoc.getY()+y];
+		}
+	}
+	
+	public boolean reachedEnd (Tile currLoc) {
+		//destination is at (width-1, height)
+		return (currLoc.getX() == width-1 && currLoc.getY() == height);
+	}
 }
--- MazeGame/src/Player.java
+++ MazeGame/src/Player.java
@@ -1,4 +1,23 @@
 
 public class Player {
-
+	private String name;
+	private int x;
+	private int y;
+	
+	public Player(String name){
+		this.name = name;
+	}
+	
+	//returns the name object
+	public String getName(){
+		return name;
+	}
+	
+	public getX(){
+		return this.x;
+	}
+	
+	public getY(){
+		return this.y;
+	}
 }
