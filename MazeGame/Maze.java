62f22f5fea68b0f1cd61af2639cad74ece7bc769
Gavin Tam <gavintam168@gmail.com>
2014-05-14 00:09:18 +1000

    Added maze gen

--- MazeGame/Maze.java
+++ MazeGame/Maze.java
@@ -1,3 +1,6 @@
+import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.Comparator;
 /**
  * Contains the board of the maze.
  * It contains the maze-generating algorithm.
@@ -13,30 +16,113 @@ public class Maze {
 	private Tile playerLoc;	//location of the current player
 			
 	public Maze (int width, int height) {
+		//width and height should be an odd number
+		//width and height is assumed to be greater than 3
 		grid = new Tile[width][height];
 		this.width = width;
 		this.height = height;
 		createMaze();	//initialise all tiles
-		playerLoc = grid[0][1];	//origin at (0,1), 
+		playerLoc = grid[1][1];	//origin at (1,1), 
 								//width and height should be big enough to allow this to be valid
 	}
 	
 	public void createMaze() {
+		//initialise maze as all non-walkable tiles
+		//and assign a random weight to each tile
+		final double weights[][] = new double[width][height];
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
-				Tile t; 
-				//if border tile
-				//not origin which is (0,1)
-				//not destination which is (width-1, height)
-				if ((i == 0 && j != 1) || (i == width-1 && j != height) ||
-					j == 0 || j == height-1) {
-					t = new Tile(false,i,j); // false denotes walkable tile
-				} else {
-					t = new Tile(true,i,j); // true denotes wall/non-walkable tile
-				}
+				Tile t = new Tile(false,i,j); // false denotes non-walkable tile
 				grid[i][j] = t;
+				weights[i][j] = Math.random();
 			}
 		}
+		//Use Prim's algorithm on a randomly weighted grid graph representation of the maze
+		//vertices are represented by tiles with odd coordinates
+		//and edges are represented by two adjacent vertices (i.e. a series of 3 consecutive tiles)
+		ArrayList<Tile> visited = new ArrayList<Tile>();
+		PriorityQueue<TileEdge> edges = new PriorityQueue<TileEdge>(100, new Comparator<TileEdge>() { 
+			public int compare (TileEdge e1, TileEdge e2) {
+				double weightDiff = (weights[e1.getTile0().getX()][e1.getTile0().getY()] +
+						weights[e1.getTile1().getX()][e1.getTile1().getY()] +
+						weights[e1.getTile2().getX()][e1.getTile2().getY()])
+						-(weights[e2.getTile0().getX()][e2.getTile0().getY()]
+						+ weights[e2.getTile1().getX()][e2.getTile1().getY()]
+						+ weights[e2.getTile2().getX()][e2.getTile2().getY()]);
+				if (weightDiff > 0) return 1;
+				if (weightDiff < 0) return -1;
+				return 0;
+			}
+		});
+		
+		ArrayList<TileEdge> neighbours = getNeighbouringEdges(grid[1][1]);
+		for (int i = 0; i < neighbours.size(); i++) {
+			edges.add(neighbours.get(i));
+		}
+		visited.add(grid[1][1]);
+		int numVertices = ((width-1)/2)*((height-1)/2);
+		//while not all vertices have been visited
+		while (visited.size() < numVertices) {
+			TileEdge curr = edges.remove();
+			if ((visited.contains(curr.getTile0()) && visited.contains(curr.getTile2()))
+				|| (!visited.contains(curr.getTile0()) && !visited.contains(curr.getTile2()))) {
+				continue;
+			}
+			if (visited.contains(curr.getTile0())) {
+				neighbours = getNeighbouringEdges(curr.getTile2());
+				visited.add(curr.getTile2());
+			} else {
+				neighbours = getNeighbouringEdges(curr.getTile0());
+				visited.add(curr.getTile0());
+			}
+			for (int i = 0; i < neighbours.size(); i++) {
+				if (!edges.contains(neighbours.get(i))) {
+					edges.add(neighbours.get(i));
+				}
+			}
+			curr.getTile0().setWalkable();		//set the edge as a walkable path
+			curr.getTile1().setWalkable();
+			curr.getTile2().setWalkable();
+		}
+	}
+	
+	public void showMaze () {
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				if (grid[i][j].isWalkable()) {
+					System.out.print("0");
+				} else {
+					System.out.print("-");
+				}
+			}
+			System.out.println();
+		}
+	}
+	
+	public ArrayList<TileEdge> getNeighbouringEdges (Tile curr) {
+		ArrayList<TileEdge> neighbouringEdges = new ArrayList<TileEdge>();
+		for (int i = 1; i < width; i+=2) {
+			for (int j = 1; j < height; j+=2) {
+				if (curr.getX()-i == 2 && curr.getY()-j == 0) {
+					TileEdge newEdge = new TileEdge(grid[i][curr.getY()],
+													grid[i+1][curr.getY()],curr);
+					neighbouringEdges.add(newEdge);
+				} else if (curr.getX()-i == -2 && curr.getY()-j == 0) {
+					TileEdge newEdge = new TileEdge(curr,grid[i-1][curr.getY()],
+													grid[i][curr.getY()]);
+					neighbouringEdges.add(newEdge);
+				} else if (curr.getX()-i == 0 && curr.getY()-j == 2) {
+					TileEdge newEdge = new TileEdge(grid[curr.getX()][j],
+													grid[curr.getX()][j+1], curr);
+					neighbouringEdges.add(newEdge);
+				} else if (curr.getX()-i == 0 && curr.getY()-j == -2) {
+					TileEdge newEdge = new TileEdge(curr, grid[curr.getX()][j-1],
+													grid[curr.getX()][j]);
+					neighbouringEdges.add(newEdge);
+				}
+			}
+		}
+		return neighbouringEdges;
 	}
 	
 	/**
@@ -44,7 +130,7 @@ public class Maze {
 	 * @param p the player
 	 * @return the tile in which the player is on
 	 */
-	public Tile findPlayer (Player p) {
+	/*public Tile findPlayer (Player p) {
 		return playerLoc;
 	}
 	
@@ -61,6 +147,6 @@ public class Maze {
 	
 	public boolean reachedEnd (Tile currLoc) {
 		//destination is at (width-1, height)
-		return (currLoc.getX() == width-1 && currLoc.getY() == height);
-	}
+		return (currLoc.getX() == width-1 && currLoc.getY() == height-1);
+	}*/
 }
