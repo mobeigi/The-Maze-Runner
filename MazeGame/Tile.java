2a91261da9545daeca48d8f01e2cc17ec0cab8af
Gavin Tam <gavintam168@gmail.com>
2014-05-14 00:10:43 +1000

    Added equals and isWalkable

---
+++ MazeGame/Tile.java
@@ -0,0 +1,56 @@
+
+public class Tile {
+	private int x;
+	private int y;
+	private boolean isWalkable;
+	
+	public Tile (boolean isWalkable, int x, int y){
+		this.x = x;
+		this.y = y;
+		this.isWalkable = isWalkable;
+	}
+
+	public void setWalkable (){
+		this.isWalkable = true;
+	}
+	
+	public boolean isWalkable (){
+		return isWalkable;
+	}
+	
+	public int getX(){
+		return this.x;
+	}
+
+	public int getY(){
+		return this.y;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (isWalkable ? 1231 : 1237);
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tile other = (Tile) obj;
+		if (isWalkable != other.isWalkable)
+			return false;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+}
