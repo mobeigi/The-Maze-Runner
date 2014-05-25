f282b9c6aefec4f1df24bc7065e274f8cc40bc3a
Gavin Tam <gavintam168@gmail.com>
2014-05-25 19:25:12 +1000

    Added enemy class

---
+++ MazeGame/src/Enemy.java
@@ -0,0 +1,29 @@
+
+public class Enemy {
+	boolean isDead;
+	Tile location;
+	
+	public Enemy (Tile t) {
+		location = t;
+	}
+	
+	public Enemy () {
+		//nothing, used by maze with no location is decided yet
+	}
+	
+	public void setDead () {
+		isDead = true;
+	}
+
+	public boolean isDead() {
+		return isDead;
+	}
+	
+	public Tile getLocation () {
+		return location;
+	}
+	
+	public void setLocation (Tile t) {
+		location = t;
+	}
+}
