6ce9257db4cad69ade39ba8051e1899f4424123b
Gavin Tam <gavintam168@gmail.com>
2014-05-29 09:37:31 +1000

    Added javadocs

--- MazeGame/src/Enemy.java
+++ MazeGame/src/Enemy.java
@@ -1,28 +1,61 @@
-
+/**
+ * Enemy against player of the maze.
+ * The enemy is able to kill the player under
+ * circumstances outlined in Maze.
+ * @author Gavin Tam
+ *
+ */
 public class Enemy {
-	boolean isDead;
-	Tile location;
+	boolean isDead;	//status (dead or alive)
+	Tile location;	//where enemy is located in the maze
 	
+	/**
+	 * Constructor for enemy.
+	 * Creates an enemy on a specific tile on the maze.
+	 * @param t a tile in the maze.
+	 */
 	public Enemy (Tile t) {
 		location = t;
 	}
 	
+	/**
+	 * Constructor for enemy.
+	 * Creates an enemy (with default field values)
+	 * if location is not decided yet.
+	 */
 	public Enemy () {
-		//nothing, used by maze with no location is decided yet
+		//nothing, used by maze when no location is decided yet
 	}
 	
+	/**
+	 * Updates the status of the enemy.
+	 * @param dead if true, the enemy is now dead,
+	 * else, the enemy is alive.
+	 */
 	public void setDead (boolean dead) {
 		isDead = dead;
 	}
 
+	/**
+	 * Checks if the enemy is dead.
+	 * @return true if the enemy is dead.
+	 */
 	public boolean isDead() {
 		return isDead;
 	}
 	
+	/**
+	 * Gets the location of the enemy in the maze.
+	 * @return the tile where the enemy is located.
+	 */
 	public Tile getLocation () {
 		return location;
 	}
 	
+	/**
+	 * Update the location of the enemy in the maze.
+	 * @param t the tile where the enemy is to be located.
+	 */
 	public void setLocation (Tile t) {
 		location = t;
 	}
