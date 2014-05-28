f7b268ab9f2ba9b56fe7d840a1ee12e785ff0864
Gavin Tam <gavintam168@gmail.com>
2014-05-29 00:57:03 +1000

    Added javadocs

--- MazeGame/src/Run.java
+++ MazeGame/src/Run.java
@@ -1,8 +1,17 @@
-
+/**
+ * Runs the maze game.
+ * Logic behind screens shown is dealt by Game.
+ * @see Game
+ */
 public class Run {
 	
+	/**
+	 * Makes a new game.
+	 * It regularly queries Game for updates in game state.
+	 * @param args unused
+	 */
 	public static void main (String args[]) {
-		//Make game
+		//Make new game
 		Game g = new Game();
 		while (true) {
 			//If not in game, do nothing (wait for play button to be clicked)
@@ -17,12 +26,10 @@ public class Run {
 			while (!g.isGameOver()) {
 				if (System.currentTimeMillis() - lastUpdateTime > 20) {	//update every 20ms
 					 g.updateMazeFrame(); //update maze
-					 g.updateScore();
-					 g.checkNextLevel();
+					 g.updateScore();	//update player score
 				
 					 lastUpdateTime = System.currentTimeMillis(); //update time
 				}
-				
 			}
 			//We have finished game, show game UI and repeat
 			g.showUI();	
