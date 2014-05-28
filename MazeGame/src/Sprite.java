6641ce8c418a227b011e3c38a2306a9d82ea226e
Gavin Tam <gavintam168@gmail.com>
2014-05-28 20:53:47 +1000

    Removed inheritance from JPanel (not needed)

--- MazeGame/src/Sprite.java
+++ MazeGame/src/Sprite.java
@@ -1,12 +1,11 @@
 import java.awt.*;
 import javax.swing.ImageIcon;
-import javax.swing.JPanel;
+
 /**
  * Create a Sprite based on provided gif sprite.
  */
-public class Sprite extends JPanel{
-	//Private Fields
-	private static final long serialVersionUID = 1L;
+public class Sprite {
+	
 	private ImageIcon sprite;
 
     public Sprite(String imageName, int x, int y) {
@@ -15,9 +14,7 @@ public class Sprite extends JPanel{
     		Image image = sprite.getImage().getScaledInstance(x, y, Image.SCALE_FAST);
     		sprite.setImage(image);
     	}
-    	catch (Exception e) {} //Empty JPanel if file not found
-    	
-    	this.setOpaque(false);	//For transparency
+    	catch (Exception e) {}
     }
     
     /**
