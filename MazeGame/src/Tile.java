3ae4fc1b8884a17e35b4deb59b4a06e7e1209127
Gavin Tam <gavintam168@gmail.com>
2014-05-30 11:54:19 +1000

    Added javadocs

--- MazeGame/src/Tile.java
+++ MazeGame/src/Tile.java
@@ -1,7 +1,8 @@
 
-/**Tile Class for maze representation
- * Contains constants for its Walkability and its object-type
- * @author
+/**
+ * Tile Class for maze representation
+ * Contains constants for its type.
+ * @author Calvin Ko, Gavin Tam
  *
  */
 public class Tile {
@@ -14,15 +15,17 @@ public class Tile {
 	public static final int SWORD = 5;
 	public static final int ICE_POWER = 6;
 	
-	private int x;
+	private int x;	//coordinates of the tile
 	private int y;
-	private boolean isWalkable;
-	private int type;
+	private boolean isWalkable;	//walkability
+	private int type;	//tile type
 	
-	/**Constructor for Tile, takes in its Walkability and it's x-y coordinates
-	 * @param isWalkable Whether or not the player can walk over the tile.
-	 * @param x X Coordinate
-	 * @param y Y Coordinate
+	/**
+	 * Constructor for Tile, takes in its walkability and its x-y coordinates.
+	 * The default type is a wall.
+	 * @param isWalkable whether or not the player can walk over the tile.
+	 * @param x the x coordinate of the tile
+	 * @param y the y coordinate of the tile
 	 */
 	public Tile (boolean isWalkable, int x, int y){
 		this.x = x;
@@ -32,20 +35,27 @@ public class Tile {
 	}
 	
 	/**
-	 * Empty Constructor
+	 * Empty Constructor for Tile.
+	 * Used when no coordinates, type or walkablility for the tile has been decided.
 	 */
 	public Tile() {
 		//do nothing
 	}
 	
+	/**
+	 * Gets the type of the tile.
+	 * Constants representing the tile type are defined in the class.
+	 * @return the type of the tile.
+	 */
 	public int getType () {
 		return type;
 	}
 	
 	
-	/**returns boolean depending on the sucesssfulness of setting type.
-	 * @param newType The new type the tile will be set with.
-	 * @return true if set, false otherwise.
+	/**
+	 * Sets the type of the tile.
+	 * @param newType the new type the tile will be set with.
+	 * @return true if setting tile type was successful, false otherwise.
 	 */
 	public boolean setType (int newType) {
 		if (newType >= 0 && newType <= 6) {
@@ -55,26 +65,43 @@ public class Tile {
 		return false;
 	}
 
-	/**Sets the tile to be walkable and it's type to PATH, the default walkable type.
-	 * 
+	/**
+	 * Sets the tile to be walkable and its type to path, the default walkable type.
 	 */
 	public void setWalkable (){
 		this.isWalkable = true;
 		this.type = PATH;	//default type is path if walkable
 	}
 	
+	/**
+	 * Checks if the tile is walkable.
+	 * @return true if the tile is walkable.
+	 */
 	public boolean isWalkable (){
 		return isWalkable;
 	}
 	
+	/**
+	 * Get the x coordinate of the tile in the maze.
+	 * @return the x coordinate of the tile.
+	 */
 	public int getX(){
 		return this.x;
 	}
 
+	/**
+	 * Get the y coordinate of the tile in the maze.
+	 * @return the y coordinate of the tile.
+	 */
 	public int getY(){
 		return this.y;
 	}
 
+	/**
+	 * Override method for hashCode.
+	 * Two equal tiles must have the same hash code.
+	 * For details on equality, see equals.
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -84,6 +111,11 @@ public class Tile {
 		return result;
 	}
 
+	/**
+	 * Override method for equals.
+	 * Two tiles are equal if they have the same
+	 * x and y coordinates.
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
